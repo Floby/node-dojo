e34777f3aaa09c380f6de194ca4d9541d9366967
Florent Jaby <fjaby@octo.com>
2013-11-22 11:46:20 +0100

    Respond with 404 on unkown GETs

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var util = require('util');
+var url = require('url');
 
 function Application (options) {
   this.server = http.createServer(this._requestHandler.bind(this));
@@ -10,8 +11,15 @@ var m = Application.prototype;
 
 
 m._requestHandler = function _requestHandler(req, res) {
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello world!');
+  var path = url.parse(req.url).pathname;
+  if (path === '/greet') {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello world!');
+  }
+  else {
+    res.statusCode = 404;
+    res.end();
+  }
 };
 
 m.start = function start(callback) {
