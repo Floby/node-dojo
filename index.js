5380e03d6a90d1ce43c0a2f23de2e2c73d68c520
Florent Jaby <fjaby@octo.com>
2013-11-22 14:53:35 +0100

    set custom header for every request

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ var express = require('express');
 function Application (options) {
   this.port = options.port;
   this.app = express();
+  this.app.use(function (req, res, next) {
+    res.header('X-Custom-Header', 'My own thing');
+    next();
+  });
   this.app.get('/greet', function (req, res) {
     res.header('Content-Type', 'text/plain');
     res.end('Hello World!');
