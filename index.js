387b2638cdd946b129a087a16fc618597db3dc95
Florent Jaby <fjaby@octo.com>
2013-11-22 12:11:13 +0100

    make use of express

--- index.js
+++ index.js
@@ -1,27 +1,18 @@
 var http = require('http');
 var util = require('util');
-var url = require('url');
+var express = require('express');
 
 function Application (options) {
-  this.server = http.createServer(this._requestHandler.bind(this));
   this.port = options.port;
+  this.app = express();
+  this.app.get('/greet', function (req, res) {
+    res.header('Content-Type', 'text/plain');
+    res.end('Hello World!');
+  });
+  this.server = http.createServer(this.app);
 }
 
 var m = Application.prototype;
-
-
-m._requestHandler = function _requestHandler(req, res) {
-  var path = url.parse(req.url).pathname;
-  if (path === '/greet') {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello world!');
-  }
-  else {
-    res.statusCode = 404;
-    res.end();
-  }
-};
-
 m.start = function start(callback) {
   this.server.listen(this.port, callback);
 };
@@ -29,6 +20,5 @@ m.start = function start(callback) {
 m.stop = function stop(callback) {
   this.server.close(callback);
 };
-
 
 module.exports = Application;
