2c35e7ed736c5354bd3ed5c50963f936304fa7a7
Florent Jaby <fjaby@octo.com>
2013-11-22 11:11:24 +0100

    make test pass for step 01

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("I'll do anything you implement me to do ;)")
+var http = require('http');
+var util = require('util');
+
+function Application (options) {
+  this.server = http.createServer(this._requestHandler.bind(this));
+  this.port = options.port;
+}
+
+var m = Application.prototype;
+
+
+m._requestHandler = function _requestHandler(req, res) {
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello world!');
+};
+
+m.start = function start(callback) {
+  this.server.listen(this.port, callback);
+};
+
+m.stop = function stop(callback) {
+  this.server.close(callback);
+};
+
+
+module.exports = Application;
