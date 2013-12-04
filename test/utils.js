75f36a6885b74c3b0b6337c4b4247c698f4510d6
Florent Jaby <fjaby@octo.com>
2013-12-04 13:53:01 +0100

    refactor test, keep it KISS

---
+++ test/utils.js
@@ -0,0 +1,22 @@
+var App = require('../');
+module.exports = {
+  server: function () {
+    var server;
+    before(function (done) {
+      server = new App({
+        port: 8081
+      });
+      server.start(done);
+    });
+    
+    after(function (done) {
+      server.stop(done);
+      server = null;
+    });
+
+    beforeEach(function (done) {
+      server.reset(done);
+    });
+    return server;
+  }
+}
