f28136c1b0e977158965355f4ad7128dd0a724b2
Florent Jaby <fjaby@octo.com>
2013-12-04 22:44:13 +0100

    send notification via websocket for each new message

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var sockjs = require('sockjs');
 var http = require('http');
 var util = require('util');
 var express = require('express');
@@ -21,17 +22,38 @@ function Application (options) {
   this.app.post('/messages', express.json(), function (req, res) {
     self._messages.push(req.body);
     res.send(201);
+
+    // notify all currently connected clients
+    var event = {event: 'newMessage', author: req.body.author, body: req.body.body};
+    self._connectedClients.forEach(function (client) {
+      client.write(JSON.stringify(event));
+    });
   });
   this.server = http.createServer(this.app);
   this._messages = [];
+
+  this._connectedClients = [];
+  this._sockjsServer = sockjs.createServer();
+  this._sockjsServer.on('connection', function (connection) {
+    self._connectedClients.push(connection);
+    connection.on('close', function() {
+      var index = self._connectedClients.indexOf(connection);
+      if(index === -1) return;
+      self._connectedClients.splice(index, 1);
+    });
+  });
 }
 
 var m = Application.prototype;
 m.start = function start(callback) {
+  this._sockjsServer.installHandlers(this.server, {prefix: '/new-messages', log: function () {}});
   this.server.listen(this.port, callback);
 };
 
 m.stop = function stop(callback) {
+  this._connectedClients.forEach(function (client) {
+    client.close();
+  })
   this.server.close(callback);
 };
 
