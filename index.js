071b70b571e36bf8b1b81957483846e2a4c8f11a
Florent Jaby <fjaby@octo.com>
2013-12-04 20:58:47 +0100

    store messages in memory for now

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var express = require('express');
 function Application (options) {
   this.port = options.port;
   this.app = express();
+  var self = this;
+
   this.app.use(function (req, res, next) {
     res.header('X-Custom-Header', 'My own thing');
     next();
@@ -13,7 +15,15 @@ function Application (options) {
     res.header('Content-Type', 'text/plain');
     res.end('Hello World!');
   });
+  this.app.get('/messages', function (req, res) {
+    res.json(self._messages);
+  });
+  this.app.post('/messages', express.json(), function (req, res) {
+    self._messages.push(req.body);
+    res.send(201);
+  });
   this.server = http.createServer(this.app);
+  this._messages = [];
 }
 
 var m = Application.prototype;
@@ -24,5 +34,10 @@ m.start = function start(callback) {
 m.stop = function stop(callback) {
   this.server.close(callback);
 };
+
+m.reset = function reset(callback) {
+  this._messages = [];
+  callback();
+}
 
 module.exports = Application;
